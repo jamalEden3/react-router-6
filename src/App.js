68edf1387a4bef15f822036986590c294dbcc3f0
JamalEden <jamaleden6@gmail.com>
2022-02-12 15:58:11 +0200

    redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom'
+
+import { useState } from 'react'
 
 // pages
-import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import ProductDetails from './pages/ProductDetails'
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
+
+  const [cartIsEmpty] = useState(true);
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -16,18 +21,17 @@ function App() {
           <Link to="/about">About</Link>
           <Link to="/products">Products</Link>
         </nav>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/products/:id">
-            <ProductDetails />
-          </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/*" element={<About />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="redirec" element={<Navigate to="/about" />} />
+          <Route 
+            path="/checkout"
+            element={cartIsEmpty ? <Navigate to="/products" /> : <p>Checkout</p>}
+          />
+        </Routes>
       </BrowserRouter>
     </div>
   )
