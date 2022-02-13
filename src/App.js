658057ed4c72f313b6225fdb1e389b414b76f5d9
JamalEden <jamaleden6@gmail.com>
2022-02-13 10:24:26 +0200

    Nested router

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about/*" element={<About />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/products/:id/*" element={<ProductDetails />} />
           <Route path="/products" element={<Products />} />
           <Route path="redirec" element={<Navigate to="/about" />} />
           <Route 
